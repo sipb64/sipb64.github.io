c4de3f1324459ee5f83a5f121017f778df83e2ee
michael <sipb64@gmail.com>
2024-11-24 18:44:40 +0100

    Test2

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    header.addEventListener('click', function() {
+        alert('Bienvenue sur mon site !');
+    });
+});
